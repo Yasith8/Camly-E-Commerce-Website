b30c036571532d2a55d7afb3a00ac4037aed6034
Yasith <yasithnawodya9@gmail.com>
2024-01-03 08:16:48 +0530

    add dynamic data to arrival page

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,48 @@ import SliderBar from './components/SliderBar'
 import About from './components/About'
 import CamCard from './components/CamCard'
 
+import img11 from './assets/cam/11.jpg'
+import img12 from './assets/cam/13.jpg'
+import img13 from './assets/cam/13.jpg'
+import img21 from './assets/cam/21.jpg'
+import img22 from './assets/cam/22.jpg'
+import img31 from './assets/cam/31.jpg'
+import img32 from './assets/cam/32.jpg'
+import img33 from './assets/cam/33.jpg'
+import img41 from './assets/cam/41.jpg'
+import img42 from './assets/cam/42.jpg'
+import img51 from './assets/cam/51.jpg'
+import img52 from './assets/cam/52.jpg'
+import img53 from './assets/cam/53.jpg'
+
+
 function App() {
+
+  const cameraShowList=[
+    {id:1,
+      model:"Canon",
+      title:"Canon EOS M50 Mark II Mirrorless Digital Camera with EF-M15-45 IS STM Lens",
+      price:"LKR 245000",
+      img1:{img11},img2:{img12},img3:{img13},
+      matirial:"EOS M50",
+      wifi:"-",
+      display:"3'",
+      view:"Electronic",
+      sensor:"CMOS",
+      lensor:"-",
+      pixel:"24.1",
+      iso:"Auto, 100 to 25600 (Extended: 100 to 51200)",
+      battery:"1 x LP-E12 Rechargeable Lithium-Ion, 7.2 VDC, 875 mAh"
+    },
+    {id:2,model:"canon",title:"Canon 3000D with Canon EF-S 18-55mm F/3.5-5.6 III",price:"LKR 145000",img1:{img21},img2:{img22},matirial:"Canon 3000D",wifi:"Yes",display:"2.7'",view:"Eye-level pentamirror",sensor:"CMOS",lensor:"18-55mm lens",pixel:"18",iso:"100-6400",battery:"LP-E10"},
+    {id:3,model:"canon",title:"Canon EOS 250D DSLR Camera with EF-S 18-55 Lens",price:"LKR 218000",img1:{img31},img2:{img32},img3:{img33},matirial:"EOS 250D",wifi:"Yes",display:"3'",view:"Optical pentamirror",sensor:"CMOS",lensor:"18-55mm lens",pixel:"24.5'",iso:"100-25600",battery:"1 x LP-E10"},
+    {id:4,model:"canon",title:"Canon EOS 200D Mark II DSLR Camera with 18-55mm Lens",price:"LKR 240000",img1:{img41},img2:{img42},matirial:"EOS 200D",wifi:"Yes",display:"3'",view:"Eye-level pentamirror",sensor:"CMOS",lensor:"18-55mm lens",pixel:"24.5'",iso:"Auto, 100 to 51200",battery:"LP-E17"},
+    {id:5,model:"Nikon",title:"Nikon D7500 DSLR Camera with 18-140mm Lens",price:"LKR 368000",img1:{img51},img2:{img52},img3:{img53},matirial:"D7500",wifi:"Yes",display:"3.2'",view:"Optical pentamirror",sensor:"CMOS",lensor:"18-140mm lens",pixel:"20.9'",iso:"100-25600",battery:"1 x EN-EL15a"},
+    {id:6,model:"Nikon",title:"Nikon D7500 DSLR Camera with 18-140mm Lens",price:"LKR 368000",img1:{img51},img2:{img52},img3:{img53},matirial:"D3500",wifi:"Yes",display:"3.2'",view:"Optical pentamirror",sensor:"CMOS",lensor:"18-140mm lens",pixel:"20.9'",iso:"100-25600",battery:"1 x EN-EL15a"},
+    {id:7,model:"Nikon",title:"Nikon D7500 DSLR Camera with 18-140mm Lens",price:"LKR 368000",img1:{img51},img2:{img52},img3:{img53},matirial:"EOS 500",wifi:"Yes",display:"3.2'",view:"Optical pentamirror",sensor:"CMOS",lensor:"18-140mm lens",pixel:"20.9'",iso:"100-25600",battery:"1 x EN-EL15a"},
+    {id:8,model:"Nikon",title:"Nikon D7500 DSLR Camera with 18-140mm Lens",price:"LKR 368000",img1:{img51},img2:{img52},img3:{img53},matirial:"D1500",wifi:"Yes",display:"3.2'",view:"Optical pentamirror",sensor:"CMOS",lensor:"18-140mm lens",pixel:"20.9'",iso:"100-25600",battery:"1 x EN-EL15a"},
+  
+  ]
   
 
   return (
@@ -17,7 +58,7 @@ function App() {
     <Header/>
     <SliderBar/> 
     <About/>
-    <CamCard/>
+    <CamCard camList={cameraShowList}/>
    </div>
   )
 }
--- src/components/CamCard.jsx
+++ src/components/CamCard.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import camera1 from '../assets/camera1.png'
 
-function CamCard() {
+function CamCard(props) {
+
+  const [itemId,setItemId]=useState(0);
+
+  const btnHandler=(id)=>{
+    setItemId(id)
+  }
+
   return (
     <div className='md:w-auto pt-[2rem] mb-[2rem] px-6 md:pt-[2rem] md:px-[4rem]'>
+      <h1 className='font-bold text-[2rem] text-center pb-5'>New Arrived Products</h1>
         <div className="grid grid-cols-3 grid-rows-4 gap-4">
-               <div className="bg-green-300 row-span-4">
-                <h2>Model</h2>
+               <div className=" row-span-4">
+                <div className='flex flex-col items-center justify-between gap-8'>
+                  {props.camList.map((i)=>(
+                    <button key={i.id} onClick={() => btnHandler(i.id)} className='px-4 py-3 rounded-lg w-full text-white font-bold text-[1rem] bg-orange-500 border-[3px] border-orange-500 hover:bg-orange-400  hover:border-orange-800'>{i.matirial}</button>
+                  ))}
+                </div>
                 
                </div>
-               <div className="bg-blue-300 row-span-4 col-start-2 row-start-1"><img src={camera1} /></div>
+               <div className="bg-blue-300 row-span-4 col-start-2 row-start-1 flex items-center justify-center"><img src={props.camList[itemId]?.img1} /></div>
                <div className=" row-span-4 col-start-3 row-start-1">
                 
                 <table className='table-auto border border-black'>
                     <tbody>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Model</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Matirial</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>WiFi</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Display</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>View Finder</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Sensor Type</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Lensor Kit</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Mega Pixel</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>ISO Range</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
-                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Battery</td><td className='border-2 p-3 border-orange-700 w-full'>Nikon</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Model</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].model : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Matirial</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].matirial : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>WiFi</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].wifi : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Display</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].display : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>View Finder</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].view : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Sensor Type</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].sensor : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Lensor Kit</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].lensor : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Mega Pixel</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].pixel : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>ISO Range</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].iso : 'No Details'}</td></tr>
+                        <tr className=''><td className='bg-orange-200 border-2 p-3 px-5 border-orange-700'>Battery</td><td className='border-2 p-3 border-orange-700 w-full'>{props.camList[itemId] ? props.camList[itemId].battery : 'No Details'}</td></tr>
                     </tbody>
                 </table>
                </div>
         </div>
+
 
     </div>
   )
