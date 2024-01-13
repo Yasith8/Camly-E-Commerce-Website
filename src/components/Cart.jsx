55a83b4f34a92b9639fff5339e87b1e5d6e04946
Yasith <yasithnawodya9@gmail.com>
2024-01-13 19:03:49 +0530

    bunch of headache bug fix in cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import CartItem from './CartItem';
-import { Modal } from 'antd';
+import React, { useEffect, useState } from "react";
+import CartItem from "./CartItem";
+import { Modal, Alert } from "antd";
+import { Style } from "@mui/icons-material";
 
-function Cart({cartCount,cameraShowList,cartIndex,arrAdd}) {
+function Cart({ cartCount, cameraShowList, cartIndex, arrAdd, afterOrder }) {
+  console.log(arrAdd);
 
-  console.log(arrAdd)
-
-  const [totalCount,setTotalCount]=useState(0)
+  const [totalCount, setTotalCount] = useState(0);
   const [cartItems, setCartItems] = useState([]);
- 
+
   useEffect(() => {
     let total = 0;
     for (let i = 0; i < arrAdd.length; i++) {
       let index = arrAdd[i];
-      let eleTot=parseInt(cameraShowList[index+1].price)
+      let eleTot = parseInt(cameraShowList[index + 1].price);
       total += eleTot;
     }
     setTotalCount(total);
   }, [arrAdd, cameraShowList]);
-
 
   const updateTotalPrice = (priceChange) => {
     setTotalCount((prevTotal) => prevTotal + priceChange);
   };
 
   const handleRemoveFromCart = (itemToRemove) => {
-    setCartItems((prevItems) => prevItems.filter((item) => item !== itemToRemove));
+    setCartItems((prevItems) =>
+      prevItems.filter((item) => item !== itemToRemove)
+    );
   };
-
 
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
@@ -35,88 +35,173 @@ function Cart({cartCount,cameraShowList,cartIndex,arrAdd}) {
   };
   const handleOk = () => {
     setIsModalOpen(false);
+    //afterOrder(0)
+    Modal.success({
+      title: "Thank You🛒",
+      content: "Your Ordering Process completed Successfully🎉✅",
+      onOk: () => {
+        setIsModalOpen(false);
+      },
+    });
   };
   const handleCancel = () => {
     setIsModalOpen(false);
   };
-  
+
   return (
     <div>
-         <ul>
+      <ul>
         {arrAdd.map((item, index) => (
-          <CartItem key={index} data={cameraShowList[index]} updateTotalPrice={updateTotalPrice} onRemove={handleRemoveFromCart}/>
+          <CartItem
+            key={index}
+            data={cameraShowList[index]}
+            updateTotalPrice={updateTotalPrice}
+            onRemove={handleRemoveFromCart}
+          />
         ))}
       </ul>
 
-      <div className='fixed bottom-0 p-6 w-full bg-white flex  items-center gap-[9rem]'>
+      <div className="fixed bottom-0 p-6 w-full bg-white flex  items-center gap-[9rem]">
         <div>
-
-              <div className='font-semibold'>Total:</div> 
-              <div className='font-bold text-[1rem]'>{totalCount}</div>
+          <div className="font-semibold">Total:</div>
+          <div className="font-bold text-[1rem]">{totalCount}</div>
         </div>
-              <div><button className='bg-orange-500 text-white font-semibold p-3 rounded-md' onClick={showModal}>Pay Now</button></div>
+        <div>
+          <button
+            className="bg-orange-500 text-white font-semibold p-3 rounded-md"
+            onClick={showModal}
+          >
+            Pay Now
+          </button>
+        </div>
+      </div>
+      <Modal
+        title="Pay Here"
+        open={isModalOpen}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{
+          style: {
+            backgroundColor: "#f97316",
+            borderColor: "#f97316",
+            color: "white",
+          },
+        }}
+        okText="Place Order"
+        width={1000}
+      >
+        <div className="flex">
+          <div className="mt-3">
+            <h1 className="font-bold text-[1rem]">Add Your Delivery Details</h1>
+            <form action="">
+              <div className="grid grid-cols-3 items-center">
+                <label htmlFor="name" className="w-10">
+                  Name:
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  className="w-[15rem] py-1 border"
+                  placeholder="Enter your Name"
+                  required
+                />
+              </div>
+
+              <div className="grid grid-cols-3 mt-2 items-center">
+                <label htmlFor="mobile">Mobile:</label>
+                <input
+                  type="text"
+                  id="mobile"
+                  className="w-[15rem] py-1 border"
+                  placeholder="Enter your Mobile Number"
+                  required
+                />
+              </div>
+
+              <div className="grid grid-cols-3 mt-2 items-center">
+                <label htmlFor="address">Address:</label>
+                <textarea
+                  id="address"
+                  className="w-[15rem] py-1 border"
+                  placeholder="Address"
+                  required
+                />
+              </div>
+            </form>
+
+            <h1 className="font-bold text-[1rem] my-2">Payment Details</h1>
+
+            <div className="grid grid-cols-3 mt-2 items-center">
+              <label htmlFor="mobile">Account Name:</label>
+              <input
+                type="text"
+                id="mobile"
+                className="w-[15rem] py-1 border"
+                placeholder="H N V PERERA"
+                required
+              />
             </div>
-            <Modal title="Pay Here" open={isModalOpen} onOk={handleOk} onCancel={handleCancel} width={1000}>
-              <div className='flex'>
-              <div className='mt-3'>
-              <h1 className='font-bold text-[1rem]'>Add Your Delivery Details</h1>
-                <form action="">
 
-                  <div className='grid grid-cols-3 items-center'>
-                  <label htmlFor="name" className='w-10'>Name:</label>
-                  <input type="text" id='name' className='w-[15rem] py-1 border' placeholder='Enter your Name'/>
-                  </div>
+            <div className="grid grid-cols-3 mt-2 items-center">
+              <label htmlFor="mobile">Account Number:</label>
+              <input
+                type="text"
+                id="mobile"
+                className="w-[15rem] py-1 border"
+                placeholder="1234 **** **** ****"
+                required
+              />
+            </div>
 
-                  
-                  <div className='grid grid-cols-3 mt-2 items-center'>
-                  <label htmlFor="mobile">Mobile:</label>
-                  <input type="text" id='mobile' className='w-[15rem] py-1 border' placeholder='Enter your Mobile Number'/>
-                  </div>
+            <div className="grid grid-cols-3 mt-2 items-center">
+              <input
+                type="text"
+                id="mobile"
+                className="w-[5rem] py-1 border"
+                placeholder="CVC"
+              />
+              <label htmlFor="mobile">Expire Date:</label>
+              <input
+                type="date"
+                id="mobile"
+                className="w-[3rem] py-1 border"
+                placeholder="1234 **** **** ****"
+                required
+              />
+            </div>
 
-
-                  <div className='grid grid-cols-3 mt-2 items-center'>
-                  <label htmlFor="address">Address:</label>
-                  <textarea id='address' className='w-[15rem] py-1 border' placeholder='Address' />
-                  </div>
-                </form>
-
-                <h1 className='font-bold text-[1rem] my-2'>Payment Details</h1>
-
-                <div className='grid grid-cols-3 mt-2 items-center'>
-                  <label htmlFor="mobile">Account Name:</label>
-                  <input type="text" id='mobile' className='w-[15rem] py-1 border' placeholder='H N V PERERA'/>
-                  </div>
-
-                  <div className='grid grid-cols-3 mt-2 items-center'>
-                  <label htmlFor="mobile">Account Number:</label>
-                  <input type="text" id='mobile' className='w-[15rem] py-1 border' placeholder='1234 **** **** ****'/>
-                  </div>
-
-                  <div className='grid grid-cols-3 mt-2 items-center'>
-                  <input type="text" id='mobile' className='w-[5rem] py-1 border' placeholder='CVC'/>
-                  <label htmlFor="mobile">Expire Date:</label>
-                  <input type="date" id='mobile' className='w-[3rem] py-1 border' placeholder='1234 **** **** ****'/>
-                  </div>
-
-
-                <div>
-
-                </div>
-              </div>
-              <div className='w-[30rem] h-[25rem] bg-orange-500 rounded-xl'>
-                <h1 className=''>Order Summery</h1>
-                
-              </div>
+            <div></div>
+          </div>
+          <div className="w-[30rem] h-[25rem] bg-orange-500 rounded-xl p-5">
+            <h1 className="text-[1.1rem] font-bold">Order Summery</h1>
+            <div>
+              <div className="flex items-center justify-between">
+                <h1>SubTotal</h1>
+                <h1>{totalCount}</h1>
               </div>
 
-
-              <div>
-                <div></div>
-                <div></div>
+              <div className="flex items-center justify-between">
+                <h1>Discount</h1>
+                <h1>-15000</h1>
               </div>
-           </Modal>
+
+              <div className="flex items-center justify-between">
+                <h1>Delivery Fee</h1>
+                <h1>+700</h1>
+              </div>
+
+              <hr />
+
+              <div className="flex items-center justify-between">
+                <h1>Total</h1>
+                <h1>{totalCount + 700 - 15000}</h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import '../components/font.css'
+import "../components/font.css";
 
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import Badge from "@mui/material/Badge/Badge"
-import { Button, Drawer } from 'antd';
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Badge from "@mui/material/Badge/Badge";
+import { Button, Drawer } from "antd";
 
-import MenuIcon from '@mui/icons-material/Menu';
-import EmptyCart from './EmptyCart';
-import Cart from './Cart';
+import MenuIcon from "@mui/icons-material/Menu";
+import EmptyCart from "./EmptyCart";
+import Cart from "./Cart";
 
-function Navbar({cartCount,shopList,cartIndex,arrAdd}) {
+function Navbar({ cartCount, shopList, cartIndex, arrAdd }) {
   const [open, setOpen] = useState(false);
-  const [openCart,setOpenCart]=useState(false);
+  const [openCart, setOpenCart] = useState(false);
   const showDrawer = () => {
     setOpen(true);
   };
@@ -20,65 +20,154 @@ function Navbar({cartCount,shopList,cartIndex,arrAdd}) {
     setOpen(false);
   };
 
+  const handleCart = () => {
+    setOpenCart(true);
+  };
 
-  const handleCart=()=>{
-    setOpenCart(true)
-  }
-  
-  const closeCart=()=>{
-    setOpenCart(false)
-  }
+  const closeCart = () => {
+    setOpenCart(false);
+  };
 
-  const itemOnClose=(data)=>{
-    setOpenCart(data)
-  }
+  const itemOnClose = (data) => {
+    setOpenCart(data);
+  };
+
+  const afterOrder = (data) => {
+    cartCount(data);
+  };
   return (
     <div>
-      <nav className='fixed top-0 left-0 w-screen bg-white'>
-        <ul className='flex items-center justify-between py-3 px-8 md:px-[5rem] '>
-          <li><h1 className='font-bold scale-150'>Camly<span className='text-orange-500 text-lg'>.</span></h1></li>
+      <nav className="fixed top-0 left-0 w-screen bg-white">
+        <ul className="flex items-center justify-between py-3 px-8 md:px-[5rem] ">
           <li>
-            <ul className='hidden md:flex md:gap-7'>
-              <li className=' text-slate-600 hover:text-black'><a href="#home">Home</a></li>
-              <li className=' text-slate-600 hover:text-black'><a href="#about">About</a></li>
-              <li className=' text-slate-600 hover:text-black'><a href="#contact">Contact</a></li>
-              <li className=' text-slate-600 hover:text-black'><a href="#blog">Blog</a></li>
+            <h1 className="font-bold scale-150">
+              Camly<span className="text-orange-500 text-lg">.</span>
+            </h1>
+          </li>
+          <li>
+            <ul className="hidden md:flex md:gap-7">
+              <li className=" text-slate-600 hover:text-black">
+                <a href="#home">Home</a>
+              </li>
+              <li className=" text-slate-600 hover:text-black">
+                <a href="#about">About</a>
+              </li>
+              <li className=" text-slate-600 hover:text-black">
+                <a href="#contact">Contact</a>
+              </li>
+              <li className=" text-slate-600 hover:text-black">
+                <a href="#blog">Blog</a>
+              </li>
             </ul>
           </li>
 
-          <li className='flex items-center justify-evenly gap-7'>
-          <Badge color="warning" badgeContent={cartCount} onClick={handleCart} className='cursor-pointer'>
-            <ShoppingCartIcon />
-          </Badge>
-          <Drawer title="My Cart" placement="right" onClose={closeCart} open={openCart}>
-            <div>
-              {(cartCount==0)? <EmptyCart itemOnClose={itemOnClose}/>:<Cart cartCount={cartCount} cameraShowList={shopList} cartIndex={cartIndex} arrAdd={arrAdd}/>}
-            </div>
-            
-          </Drawer>
-          <button className='bg-orange-500 text-white py-2 px-5 rounded-md'>LogIn</button>
+          <li className="flex items-center justify-evenly gap-7">
+            <Badge
+              color="warning"
+              badgeContent={cartCount}
+              onClick={handleCart}
+              className="cursor-pointer"
+            >
+              <ShoppingCartIcon />
+            </Badge>
+            <Drawer
+              title="My Cart"
+              placement="right"
+              onClose={closeCart}
+              open={openCart}
+            >
+              <div>
+                {cartCount == 0 ? (
+                  <EmptyCart itemOnClose={itemOnClose} />
+                ) : (
+                  <Cart
+                    cartCount={cartCount}
+                    cameraShowList={shopList}
+                    cartIndex={cartIndex}
+                    arrAdd={arrAdd}
+                    afterOrder={afterOrder}
+                  />
+                )}
+              </div>
+            </Drawer>
+            <button className="bg-orange-500 text-white py-2 px-5 rounded-md">
+              LogIn
+            </button>
 
-          {/* hamburger menu need to be done */}
-          <div className='md:hidden'>
-          <button onClick={showDrawer}>
-        <MenuIcon/>
-      </button>
-      <Drawer title="Capture List" placement="right" onClose={onClose} open={open}>
-        <ul className='py-5 flex flex-col items-center gap-10'>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#home">Home</a></li>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#about">About Us</a></li>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#latest">Our Latest Products</a></li>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#card">Shop</a></li>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#contact">Contact</a></li>
-          <li><a className='text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400' onClick={onClose} href="#blog">Blog</a></li>
-        </ul>
-      </Drawer>
-          </div>
+            {/* hamburger menu need to be done */}
+            <div className="md:hidden">
+              <button onClick={showDrawer}>
+                <MenuIcon />
+              </button>
+              <Drawer
+                title="Capture List"
+                placement="right"
+                onClose={onClose}
+                open={open}
+              >
+                <ul className="py-5 flex flex-col items-center gap-10">
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#home"
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#about"
+                    >
+                      About Us
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#latest"
+                    >
+                      Our Latest Products
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#card"
+                    >
+                      Shop
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#contact"
+                    >
+                      Contact
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="text-[18px] p-3 text-black hover:text-white rounded-lg hover:bg-orange-400"
+                      onClick={onClose}
+                      href="#blog"
+                    >
+                      Blog
+                    </a>
+                  </li>
+                </ul>
+              </Drawer>
+            </div>
           </li>
         </ul>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
